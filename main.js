7a5d80cdca54409b91089b1dd534a46be409c6b4
Cielle Carlton <cielle.carlton13@gmail.com>
2020-10-13 16:08:43 -0400

    resubmitting nbad homework assignment

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 // take in user input 
 var inquirer = require('inquirer');
 var fs = require("fs");
-inquirer
-  .prompt([
+
+
+inquirer.prompt([
     {
-      message: "What is your name?",
+      message: "What is the name of your project?",
       type: "input",
       name: "projectName",
 
     },
     {
-      message: "What is the name of your project?",
+      message: "What is your name?",
       type: "input",
       name: "name",
 
@@ -65,18 +66,45 @@ inquirer
   type: "input"
 }
 
+ ])
+
+//  answers to go into a template and generate the readme info
+
+  .then (answers => {
+    var markdown =`
+# ${answers.projectName}
+
+## Name
+    ${answers.name}
+    
+## Description
+    ${answers.description}
+    
+### Install Instructions
+    ${answers.installation}
+    
+### Contributors
+    ${answers.contributers}
+    
+### Useage Info
+    ${answers.usage}
+    
+### Licenese
+    ${answers.license}
+    
+### Run Test Command
+    ${answers.test}
+    
+## Github Username
+    ${answers.username}
+    
+## Email Address
+    ${answers.email}`
 
 
 
-
-
-  ])
-
-
-
-
-  .then(answers => {
-    fs.writeFile("readme.md", answers, function (err, data) {
+    fs.writeFile("readme.md", markdown, function (err, data) {
+      console.log('file written')
     // console.log(answers.description)
     // Use user feedback for... whatever!!
     })
--- readme.md
+++ readme.md
@@ -1,1 +1,29 @@
-[object Object]
+
+# cielle
+
+## Name
+    cielle
+    
+## Description
+    cute
+    
+### Install Instructions
+    node index.js
+    
+### Contributors
+    undefined
+    
+### Useage Info
+    awesome
+    
+### Licenese
+    MIT
+    
+### Run Test Command
+    npm run test
+    
+## Github Username
+    ciellecarlton
+    
+## Email Address
+    cielle.carlton13@gmail.com
